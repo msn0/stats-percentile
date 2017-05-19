4b2f21a03303521072fa02ac0476b7ddcfdbad8c
Michal Jezierski <ambinanitelo@gmail.com>
2017-05-19 11:58:25 +0200

    eslint fixes

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 function swap(data, i, j) {
-  if (i === j) {
-    return;
-  }
-  const tmp = data[j];
-  data[j] = data[i];
-  data[i] = tmp;
+    if (i === j) {
+        return;
+    }
+    const tmp = data[j];
+    data[j] = data[i];
+    data[i] = tmp;
 }
 
 function partition(data, start, end) {
-  let i, j;
-  for (i = start + 1, j = start; i < end; i++) {
-    if (data[i] < data[start]) {
-      swap(data, i, ++j);
+    let i, j;
+    for (i = start + 1, j = start; i < end; i++) {
+        if (data[i] < data[start]) {
+            swap(data, i, ++j);
+        }
     }
-  }
-  swap(data, start, j);
-  return j;
+    swap(data, start, j);
+    return j;
 }
 
 function findK(data, start, end, k) {
-  while (start < end) {
-    let pos = partition(data, start, end);
-    if (pos === k) {
-      return data[k];
+    while (start < end) {
+        let pos = partition(data, start, end);
+        if (pos === k) {
+            return data[k];
+        }
+        if (pos > k) {
+            end = pos;
+        } else {
+            start = pos + 1;
+        }
     }
-    if (pos > k) {
-      end = pos;
-    } else {
-      start = pos + 1;
-    }
-  }
 }
 
 // Calculate n-th percentile of 'data' using Nearest Rank Method
